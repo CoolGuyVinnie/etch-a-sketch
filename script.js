58381288661aef9f6e897939037902133b9fb12b
Vincent <Nguyen.Vincent11@Gmail.com>
2024-05-10 13:18:10 -0700

    Testing with an additional square

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const container = document.querySelector("#container");
 
 // Creates new element of tag type (only creates in memory)
 const square = document.createElement("div");
+const square1 = document.createElement("div");
 
 // Apply styling to square
 square.classList.add("square");
+square1.classList.add("square");
 
-// Content to provide in HTML
+// Changes the styling element for square
 square.addEventListener("mouseenter", function() {
     this.style.backgroundColor = "red";
 })
@@ -16,5 +18,14 @@ square.addEventListener("mouseleave", function() {
     this.style.backgroundColor = "aquamarine";
 })
 
+square1.addEventListener("mouseenter", function() {
+    this.style.backgroundColor = "red";
+})
+
+square1.addEventListener("mouseleave", function() {
+    this.style.backgroundColor = "aquamarine";
+})
+
 // Applies element in HTML
 container.append(square);
+container.append(square1);
