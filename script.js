5bf2cb338f1e5901dc5f160cd372fc8a3e456632
Vincent <Nguyen.Vincent11@Gmail.com>
2024-05-18 15:32:08 -0700

    Refactored to create rows, then append columns to each row

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 // Selects "container" in HTML file
 const container = document.querySelector("#container");
-const containercols = document.querySelector("#containercols");
 
 // Initializers
 const rows = [];
-const columns = [];
 
 // Will need to change "i" to be user prompt for number of square
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 2; i++) {
     // Creates new element of tag type (only creates in memory)
     rows[i] = document.createElement("div");
-    columns[i] = document.createElement("div");
 
-    // Apply Styling to square
-    rows[i].classList.add("square");
-    columns[i].classList.add("square");
+    // Creates rows
+    rows[i].classList.add("row");
 
-    // Changes the styling for square
-    rows[i].addEventListener("mouseenter", function() {
-        this.style.backgroundColor = "red";
-    })
-    columns[i].addEventListener("mouseenter", function() {
-        this.style.backgroundColor = "red";
-    })
+    // Creates columns
+    for (let j = 0; j < 3; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
 
-    rows[i].addEventListener("mouseleave", function() {
-        this.style.backgroundColor = "blue";
-    })
-    columns[i ].addEventListener("mouseleave", function() {
-        this.style.backgroundColor = "blue";
-    })
+        // Changes the styling for square
+        square.addEventListener("mouseenter", function() {
+            this.style.backgroundColor = "red";
+        })
+
+        square.addEventListener("mouseleave", function() {
+            this.style.backgroundColor = "blue";
+        })
+        rows[i].append(square);
+    }
 
     // Applies element into HTML
     container.append(rows[i]);
-    containercols.append(columns[i]);
 }
